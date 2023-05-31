973e2afb1068430b3ace0b928b211de45b75f9a0
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-31 12:53:50 +0600

    Using axios interceptor by using custom useAxiosSecure hook

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,19 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
 const useCart = () => {
   const { user } = useContext(AuthContext);
+  // const token = localStorage.getItem("access-token");
+  const [axiosSecure] = useAxiosSecure();
+
   const {
     isLoading,
     refetch,
     data: cart = [],
   } = useQuery({
     queryKey: ["carts", user?.email],
+    // queryFn: async () => {
+    //   const res = await fetch(
+    //     `http://localhost:5000/carts?email=${user.email}`,
+    //     {
+    //       headers: {
+    //         authorization: `Bearer ${token}`,
+    //       },
+    //     }
+    //   );
+    //   return res.json();
+    // },
     queryFn: async () => {
-      const res = await fetch(
-        `http://localhost:5000/carts?email=${user.email}`
-      );
-      return res.json();
+      const res = await axiosSecure(`/carts?email=${user.email}`);
+      console.log(res);
+      return res.data;
     },
   });
   return [cart, refetch, isLoading];
